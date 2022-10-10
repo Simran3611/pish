d5a124fd9eb14a89b4a8fd11cc2af2fa03d2c7b9
Simran3611 <98481310+Simran3611@users.noreply.github.com>
2022-10-10 18:45:43 -0500

    Added the comments to the pish.c

---
+++ pish.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main() {
+
+  //while the user says y to entering the commands we execute the loop
+  //break down the command and uses the redirection tokens (<, >, >>, |)
+  //at the end of the loop it would ask again if the user wants to enter a command
+
+
+  return 0;
+
+}
