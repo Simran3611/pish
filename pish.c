28a0f59ac6ed732465c7a8e4d09d4a70749e2615
Simran3611 <98481310+Simran3611@users.noreply.github.com>
2022-10-25 22:19:03 -0500

    Added things
    Parsing

--- pish.c
+++ pish.c
@@ -17,60 +17,132 @@ struct command {
 
 
 
-int checkRedirection(char *buffer) {
-  for(int i = 0; i < sizeof(buffer); i++) {
-    char c = buffer[i];
-    if(&c == "<"); {
-      printf("Redirect to standard Input\n");
-      return i;
-      //add a method redirectionToStdIn;
-    }
-    if(&c == ">") {
-      printf("Redirect the standard output to a file\n");
-      return i;
-    //add a method that creates and writes to a file. Overrides the contents of a file
-    }
-    if(&c == ">") {
-	if(&buffer[i+1] == ">") {
-      		printf("Redirect output to file without overriding contents\n");
-		return (i+1);
-	}
-    }
-    //add a method that adds to contents of a file.
-  }
-}
-
-void executeTextFile(int num, char *buffer) {
-    char temp[100];
-    for(int j = num; num < sizeof(buffer); num++){
-	temp[j] = buffer[num];
-    }
-    printf("redirection successful");
-    
+void outputRedir(char *found) {
+    //printf("File redirection successful\n");
     //redirecting file using dup2. FIX
-    //int redirect_fd = open("redirect_fd.txt", O_CREAT | O_TRUNC | O_WRONLY);
-    //dup2(redirect_fd, STDOUT_FILENO);
-    //close(redirect_fd);
+    int redirect_fd = open(found, O_CREAT | O_TRUNC | O_WRONLY);
+    dup2(redirect_fd, STDOUT_FILENO);
+    close(redirect_fd);
+}
+void appendRedir(char *found) {
+    //printf("File redirection successful\n");
+    //redirecting file using dup2. FIX
+    int redirect_fd = open(found, O_CREAT | O_TRUNC | O_WRONLY);
+    dup2(redirect_fd, STDOUT_FILENO);
+    close(redirect_fd);
 }
 
-void parse(char* buffer) {
-	//DOESN'T WORK 
-	char* piece = strtok(buffer, "<");
-	//printf("Prints strtok %s\n", piece);
-	piece = strtok(buffer, "<;");
-	piece = strtok(buffer, "<;");
-	printf("Prints strtok %s\n", piece);
+void parseSpace(char* buffer) {
+    	//char str[] = "strtok needs to be called several times to split a string";
+	int init_size = strlen(buffer);
+	char delim[] = " ";
+
+	char *ptr = strtok(buffer, delim);
+
+	while(ptr != NULL)
+	{
+		printf("'%s'\n", ptr);
+		ptr = strtok(NULL, delim);
+	}
+	printf("\n");
+
 }
+void parseOutputRedir(char* buffer) {
+    	//char str[] = "strtok needs to be called several times to split a string";
+	int init_size = strlen(buffer);
+	char delim[] = ">";
+
+	char *ptr = strtok(buffer, delim);
+
+	while(ptr != NULL)
+	{
+		printf("'%s'\n", ptr);
+		ptr = strtok(NULL, delim);
+	}
+	printf("\n");
+
+}
+
+void writeInputRedir(char* buffer, char* ptr) {
+	int fd;
+	int ret;
+	fd = open(ptr, O_CREAT | O_APPEND | O_WRONLY);
+	if(fd < 0) {
+		perror("open");
+		exit(1);
+	}
+	ret = dup2(fd, 1);
+	if(ret < 0) {
+		perror("dup2");
+		exit(1);
+	}
+	execl("/bin/sh", "/bin/sh", "-c", buffer, 0);
+	close(fd);
+	//return;
+}
+
+void parseInputRedir(char* buffer) {
+	FILE *fp;
+    	//char str[] = "strtok needs to be called several times to split a string";
+	int init_size = strlen(buffer);
+	char delim[] = "<";
+
+	char *ptr = strtok(buffer, delim);
+	int i = 0;
+	while(ptr != NULL)
+	{
+		printf("'%s'\n", ptr);
+		//i++;
+		ptr = strtok(NULL, delim);
+		printf("'%s'\n", ptr);
+
+		//printf("'%s'\n", ptr);
+	}
+	printf("\n");
+
+}
+
+void parse(char* buffer, struct command *cmd) {
+    cmd->argc = 0;	
+    char *found;
+    int i = 0;
+
+    found = strtok(buffer,"<");
+    if(found==NULL)
+    {
+        puts("\tNo separators found");
+	cmd->argv[i] = NULL;
+        //return(1);
+    }
+    int temp = 0;
+    while(found)
+    {
+        found = strtok(NULL,"<");
+	temp++;
+	cmd->argv[i] = found;
+	if(temp == 1) {
+		//printf("File: %s\n",found);
+		outputRedir(found);
+		return;
+	}
+	i++;
+    }
+
+}
+
 
 int main(int argc, char* argv[]) {
+
   while (1) {
     char buffer[100];
     char* cmdLine;
 
+    struct command *cmd;
+
     int pid = vfork();
 
     //printf("Some instructions at beginning\n");
-     if(strcmp(buffer, "quit") == 0){
+     if(strcmp(buffer, "exit") == 0){
          printf("You have quitted the process\n");
          exit(1);
       }
@@ -82,11 +154,11 @@ int main(int argc, char* argv[]) {
      //prompts the user
       //printf("%s", prompt);
       scanf("%[^\n]%*c", buffer);
-      if(strcmp(buffer, "quit") != 0){
-        printf("Your command: %s\n", buffer);
-	parse(buffer);
+      if(strcmp(buffer, "exit") != 0){
+	printf("Your command: %s\n", buffer);
+	parseInputRedir(buffer);
 	execl("/bin/sh", "/bin/sh", "-c", buffer, 0);
-	printf("%s", prompt);
+
 	//exit(1);
       }
       //execl("/bin/sh", "/bin/sh", "-c", buffer, 0);
